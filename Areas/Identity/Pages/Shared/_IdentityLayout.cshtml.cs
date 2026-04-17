2d046a2792428e611b64f2a14ec1f06ec31554af
wils0nalves <yuri.tinocoalves13@hotmail.com>
2026-04-17 20:28:11 -0300

    Tela de login corrigida, agora não tem mais aquela barra superior (que deveria de fato ser acessivel após o login)

---
+++ Areas/Identity/Pages/Shared/_IdentityLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SistemaFinanceiro.Areas.Identity.Pages.Shared
+{
+    public class _IdentityLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
